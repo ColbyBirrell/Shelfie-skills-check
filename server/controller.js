4e82ee1883615173e8b2238ee1924c159cc538a6
ColbyBirrell <colby.birrell@gmail.com>
2020-02-14 12:49:57 -0700

    get works

--- server/controller.js
+++ server/controller.js
@@ -1,23 +1,35 @@
-const axios = require("axios");
+// const axios = require("axios");
 
-const inventory = [
-  {
-    id: 55,
-    name: "10Testprod10",
-    price: 43,
-    img: "43imgtesturl43"
-  },
+// const inventory = [
+//   {
+//     id: 55,
+//     name: "10Testprod10",
+//     price: 43,
+//     img: "43imgtesturl43"
+//   },
 
-  {
-    id: 57,
-    name: "33Testprod33",
-    price: 99,
-    img: "44imgtesturl44"
-  }
-];
+//   {
+//     id: 57,
+//     name: "33Testprod33",
+//     price: 99,
+//     img: "44imgtesturl44"
+//   }
+// ];
 
 module.exports = {
-  getInventoryList(req, res) {
-    res.status(200).send(inventory);
+  getInventoryList: (req, res) => {
+    const dbInstance = req.app.get("db");
+
+    dbInstance
+      .get_inventory()
+      .then(items => {
+        res.status(200).send(items);
+      })
+      .catch(err => {
+        res.status(500).send({ errorMessage: "this done broke" });
+        console.log(err);
+      });
   }
 };
+
+// res.status(200).send(inventory);
--- src/App.js
+++ src/App.js
@@ -3,26 +3,35 @@ import "./App.css";
 import Dashboard from "./components/dashboard/Dashboard";
 import Form from "./components/form/Form";
 import Header from "./components/header/Header";
+import axios from "axios";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       inventoryList: [
-        {
-          id: 50,
-          name: "Testprod",
-          price: 27,
-          img: "imgtesturl"
-        },
-        {
-          id: 52,
-          name: "2Testprod2",
-          price: 42,
-          img: "2imgtesturl2"
-        }
+        // {
+        //   id: 50,
+        //   name: "Testprod",
+        //   price: 27,
+        //   img: "imgtesturl"
+        // },
+        // {
+        //   id: 52,
+        //   name: "2Testprod2",
+        //   price: 42,
+        //   img: "2imgtesturl2"
+        // }
       ]
     };
+  }
+
+  componentDidMount() {
+    axios.get(`/api/inventory`).then(res => {
+      this.setState({
+        inventoryList: res.data
+      });
+    });
   }
 
   render() {
